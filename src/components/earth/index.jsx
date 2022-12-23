678d97fad9fbfcf83be4e7c92e890d5350dd10c9
Niraj Rana <71956273+nirajmohanrana@users.noreply.github.com>
2022-12-23 17:57:23 +0530

    earth completed

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useLoader } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import * as THREE from "three";
 
@@ -16,17 +16,29 @@ export function Earth(props) {
     [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
   );
 
+  const earthRef = useRef();
+  const cloudsRef = useRef();
+
+  useFrame(({ clock }) => {
+    const elaspedTime = clock.getElapsedTime();
+
+    earthRef.current.rotation.y = elaspedTime / 6;
+    cloudsRef.current.rotation.y = elaspedTime / 6;
+  });
+
   return (
     <>
-      <ambientLight
-        intensity={1}
+      {/* <ambientLight intensity={1} /> */}
+      <pointLight color="#f6f3ea" position={[2, 0, 5]} intensity={1.2} />
+      <Stars
+        radius={300}
+        depth={60}
         count={20000}
         factor={7}
         saturation={0}
         faded={true}
       />
-      <Stars radius={300} depth={60} />
-      <mesh>
+      <mesh ref={cloudsRef} position={[0, 0, 3]}>
         <sphereGeometry args={[1.005, 32, 32]} />
         <meshPhongMaterial
           map={cloudsMap}
@@ -37,17 +49,22 @@ export function Earth(props) {
         />
       </mesh>
 
-      <mesh>
+      <mesh ref={earthRef} position={[0, 0, 3]}>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
-        <meshStandardMaterial map={colorMap} normalMap={normalMap} s />
-        <OrbitControls
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
+        {/* <OrbitControls
           enableZoom={true}
           enablePan={true}
           enableRotate={true}
           zoomSpeed={0.6}
           rotateSpeed={0.4}
-        />
+        /> */}
       </mesh>
     </>
   );
