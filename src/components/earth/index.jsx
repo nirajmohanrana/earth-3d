6793a57124f1588ff62ae1576ae535376c4ff02b
Niraj Rana <71956273+nirajmohanrana@users.noreply.github.com>
2022-12-23 17:40:02 +0530

    earth appeared

---
+++ src/components/earth/index.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useLoader } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+import * as THREE from "three";
+
+import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
+import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
+import EarthNightMap from "../../assets/textures/8k_earth_nightmap.jpg";
+import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
+import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
+import { TextureLoader } from "three";
+
+export function Earth(props) {
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
+    TextureLoader,
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
+  );
+
+  return (
+    <>
+      <ambientLight
+        intensity={1}
+        count={20000}
+        factor={7}
+        saturation={0}
+        faded={true}
+      />
+      <Stars radius={300} depth={60} />
+      <mesh>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshPhongMaterial
+          map={cloudsMap}
+          opacity={0.4}
+          depthWrite={true}
+          transparent={true}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+
+      <mesh>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial map={colorMap} normalMap={normalMap} s />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          rotateSpeed={0.4}
+        />
+      </mesh>
+    </>
+  );
+}
